de7de92cfc9aa412aace77d4b3569e21abc973d5
Mark Alston <malston@pivotal.io>
2016-08-31 23:05:11 -0400

    Add turbine

--- scripts/create_services.sh
+++ scripts/create_services.sh
@@ -1,3 +1,5 @@
-cf cups config-server -p '{"uri":"https://config-server-conglomeratic-packman.apps.indigo.springapps.io/"}'
-cf cups service-registry -p '{"uri":"https://eureka-unpropounded-serialization.apps.indigo.springapps.io/"}'
+cf cups config-server -p '{"uri":"https://config-server-martensitic-calcitonin.cfapps.pez.pivotal.io/"}'
+cf cups service-registry -p '{"uri":"https://eureka-obovoid-cetane.cfapps.pez.pivotal.io/"}'
+cf cs p-mysql 100mb-dev fortunes-db
+cf cs p-rabbitmq standard fortunes-amqp
 #cf cs elephantsql turtle fortunes-db
