b442c582d474b9313e4e7e4c7d3990fe4f2a5d08
Mark Alston <malston@pivotal.io>
2016-08-31 23:22:43 -0400

    Update to stream version of turbine

--- fortune-teller-turbine/src/main/java/io/spring/cloud/samples/fortuneteller/turbine/Application.java
+++ fortune-teller-turbine/src/main/java/io/spring/cloud/samples/fortuneteller/turbine/Application.java
@@ -2,10 +2,12 @@ package io.spring.cloud.samples.fortuneteller.turbine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cloud.netflix.turbine.EnableTurbine;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.netflix.turbine.stream.EnableTurbineStream;
 
 @SpringBootApplication
-@EnableTurbine
+@EnableTurbineStream
+@EnableDiscoveryClient
 public class Application {
 
 	public static void main(String[] args) {
